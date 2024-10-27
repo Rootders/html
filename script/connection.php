d9bb5a5f2309af71e3a2388edd0d82fc6342fc04
Rootders <99789132+Rootders@users.noreply.github.com>
2024-10-27 01:36:10 +0000

    k30

--- script/connection.php
+++ script/connection.php
@@ -1,4 +1,15 @@
 <?php
+$host = 'localhost';
+$user = 'root';
+
+$link = mysqli_connect($host, $user);
+
+mysqli_select_db($link, 'test_project');
+
+if(!$link){
+        die('Error connect to DataBase');
+}
+
 function connection(){
 session_start();
 $host = 'localhost';
