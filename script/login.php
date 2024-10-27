ca005cc0e61365f3c52dc682bb205c050c886a41
Rootders <99789132+Rootders@users.noreply.github.com>
2024-10-27 02:14:57 +0000

    k34

--- script/login.php
+++ script/login.php
@@ -1,21 +1,9 @@
 <?php
-session_start();
-$host = 'localhost';
-$user = 'phpmyadmin';
-$pass = 'toor';
-
-$link = mysqli_connect($host, $user, $pass);
-
-mysqli_select_db($link, 'phpmyadmin');
-
-if(!$link){
-    die('Error connect to DataBase');
-}
 $login = $_POST['login'];
 $password = $_POST['password'];
 
 $check_user = "SELECT * FROM users WHERE `login` = '$login' AND `password` = '$password'";
-$result = mysqli_query($link, $check_user);
+$result = mysqli_query(connection(), $check_user);
 
 if (mysqli_num_rows($result)>0){
     $user = mysqli_fetch_assoc($result);
